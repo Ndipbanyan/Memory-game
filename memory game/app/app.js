15329a3de520403e6c72cb2fe37158465de97464
Ndipbanyan <67186679+Ndipbanyan@users.noreply.github.com>
2020-06-20 10:41:31 +0100

    Add files via upload

---
+++ memory game/app/app.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded',() =>{
+
+    //cardoptions
+    const cardArray =[
+        {
+            name:'apple',
+            img:'images/fries.jpg'
+        },
+
+        {
+            name:'melon',
+            img:'images/melon.jpg'
+        },
+        {
+            name:'orange',
+            img:'images/orange.jpg'
+        },
+        {
+            name:'straw',
+            img:'images/straw.jpg'
+        },
+
+    
+
+    ]
+    
+    const grid = document.querySelector('.grid')
+    //create board
+})
